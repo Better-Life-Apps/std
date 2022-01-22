c1a435296c0c525b46fe81a22506bf2535e9d063
IvanPletinskiy <Ivan.Pliatsinski@effective-soft.com>
2022-01-22 22:22:41 +0300

    Add BaseFragment.setupToolbar()

--- src/main/java/com/betterlifeapps/std/BaseFragment.kt
+++ src/main/java/com/betterlifeapps/std/BaseFragment.kt
@@ -1,7 +1,21 @@
 package com.betterlifeapps.std
 
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
-abstract class BaseFragment : Fragment() {
+abstract class BaseFragment(@LayoutRes layoutRes: Int) : Fragment(layoutRes) {
 
+    protected val supportActionBar: ActionBar?
+        get() = (requireActivity() as? AppCompatActivity)?.supportActionBar
+
+    protected fun setupActionBar(actionBarInitializer: ActionBar.() -> Unit) {
+        supportActionBar?.apply {
+            setHomeAsUpIndicator(null)
+            setDisplayHomeAsUpEnabled(false)
+            title = ""
+            actionBarInitializer()
+        }
+    }
 }
