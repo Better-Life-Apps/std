9c6d9d91eb770fcbd50deaf702782f739f6b4278
Ivan Pletinskij <ivan@luno.com>
2022-03-29 15:51:20 +0300

    Add BaseViewModel.runCoroutine

--- src/main/java/com/betterlifeapps/std/BaseViewModel.kt
+++ src/main/java/com/betterlifeapps/std/BaseViewModel.kt
@@ -1,6 +1,16 @@
 package com.betterlifeapps.std
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 abstract class BaseViewModel : ViewModel() {
+
+    protected fun runCoroutine(
+        context: CoroutineContext = EmptyCoroutineContext,
+        block: suspend CoroutineScope.() -> Unit
+    ) = viewModelScope.launch(context, block = block)
 }
