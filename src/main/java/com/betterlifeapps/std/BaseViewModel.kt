ef7dc7d09af25b487ff47538ad9f8bc955829f39
Ivan Pletinskij <ivan@luno.com>
2022-03-30 12:04:45 +0300

    Add BaseViewModel.collectFlow

--- src/main/java/com/betterlifeapps/std/BaseViewModel.kt
+++ src/main/java/com/betterlifeapps/std/BaseViewModel.kt
@@ -5,6 +5,9 @@ import androidx.lifecycle.viewModelScope
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 abstract class BaseViewModel : ViewModel() {
@@ -13,4 +16,10 @@ abstract class BaseViewModel : ViewModel() {
         context: CoroutineContext = EmptyCoroutineContext,
         block: suspend CoroutineScope.() -> Unit
     ) = viewModelScope.launch(context, block = block)
+
+    /**
+     * Terminal flow operator that launches the collection of the given flow with a provided [action]
+     */
+    protected fun <T> Flow<T>.collectFlow(action: suspend (value: T) -> Unit): Job =
+        viewModelScope.launch { collect(action) }
 }
